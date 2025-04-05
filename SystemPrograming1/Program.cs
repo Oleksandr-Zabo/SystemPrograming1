ed946c02e30151355fcf8c72de46ebd3c8095e29
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-04-05 13:14:59 +0300

    ex. 1 had done

--- SystemPrograming1/Program.cs
+++ SystemPrograming1/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace SystemPrograming1;
+using System.Diagnostics;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter time for update in seconds:");
+        double time = Convert.ToDouble(Console.ReadLine());
+        int timeMilisecond = (int)(time * 1000);
+
+        while (true)
+        {
+            GetProcesses();
+            Thread.Sleep(timeMilisecond);
+        }
+    }
+    
+    static void GetProcesses()
+    {
+        Console.Clear();
+        foreach(Process process in Process.GetProcesses())
+        {
+            Console.WriteLine($"ID: {process.Id}  Name: {process.ProcessName}");
+        }
     }
 }
+
